24877f5e71bda9873683a83a3348f740fa5f7532
Sebastian Zimmermann <sebastian.zimmermann@pta.de>
2023-01-19 15:27:49 +0100

    Initial commit

---
+++ src/main/java/com/pta/ptaspringbackend/PtaspringbackendApplication.java
@@ -0,0 +1,13 @@
+package com.pta.ptaspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtaspringbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtaspringbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pta/ptaspringbackend/PtaspringbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pta.ptaspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtaspringbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
